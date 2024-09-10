cc049f4aa893e9d08f7561917d4b453c9e9f8a46
rotent <93746829+rotent@users.noreply.github.com>
2024-09-10 20:22:18 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -70,6 +70,7 @@ download_success=false
 
 # 代理列表
 proxy_zip_urls="
+https://raw.githubusercontent.com/rotent/tailscale-openwrt/chinese_mainland/tailscale-openwrt.tgz
 https://raw.githubusercontent.com/CH3NGYZ/tailscale-openwrt/chinese_mainland/tailscale-openwrt.tgz
 https://ghproxy.net/https://raw.githubusercontent.com/CH3NGYZ/tailscale-openwrt/chinese_mainland/tailscale-openwrt.tgz
 https://fastly.jsdelivr.net/gh/CH3NGYZ/tailscale-openwrt@chinese_mainland/tailscale-openwrt.tgz
